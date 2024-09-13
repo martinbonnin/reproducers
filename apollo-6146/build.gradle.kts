ebb43dcf96c0e58d1ff29ecc3e99151b0cac7110
Martin Bonnin <martin@mbonnin.net>
2024-09-13 10:37:11 +0200

    Add reproducer

---
+++ apollo-6146/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+  id("com.android.library").version("8.5.0")
+  id("org.jetbrains.kotlin.android").version("2.0.20")
+  id("com.apollographql.apollo").version("4.0.0")
+  id("maven-publish")
+}
+
+dependencies {
+  implementation("com.apollographql.apollo:apollo-api")
+}
+
+apollo {
+  service("service") {
+    packageName.set("com.example")
+  }
+}
+
+android {
+  namespace = "com.example"
+  defaultConfig {
+    minSdk = 24
+    compileSdk = 34
+  }
+
+  publishing {
+    singleVariant("release") {
+      withSourcesJar()
+    }
+  }
+
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+group = "com.example"
+version = "0.0.0"
+
+publishing {
+  publications {
+    create("lib", MavenPublication::class.java) {
+
+      afterEvaluate {
+        from(components.getByName("release"))
+      }
+    }
+  }
+}
+
+
